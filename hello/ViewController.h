1ca4ba13e96519d7d74f986ede352d542666ac2f
robertoreym <roberto.rey@smartplace.mx>
2015-05-21 15:26:11 -0500

    Initial Commit

---
+++ hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by Roberto Rey Magaña on 21/05/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hello/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  hello
+//
+//  Created by Roberto Rey Magaña on 21/05/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
